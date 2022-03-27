7de44625d376f79a40bddb6824675c8eec5cb51c
Abhishek Chaubey <90348147+abhi98chaubey@users.noreply.github.com>
2022-03-27 08:33:01 +0530

    Time: 159 ms (24.08%), Space: 88.7 MB (29.87%) - LeetHub

---
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.java
@@ -0,0 +1,28 @@
+class OrderedStream {
+    Map<Integer,String> map = new HashMap<>();
+    int ptr;
+    int length;
+
+    public OrderedStream(int n) {
+        length = n;
+        ptr = 1;
+    }
+    
+    public List<String> insert(int idKey, String value) {
+        List<String> list = new ArrayList<>();
+        
+        map.put(idKey,value);
+        while(map.containsKey(ptr)) {
+            list.add(map.get(ptr));
+            ptr++;
+        }
+        
+        return list;
+    }
+}
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * OrderedStream obj = new OrderedStream(n);
+ * List<String> param_1 = obj.insert(idKey,value);
+ */
