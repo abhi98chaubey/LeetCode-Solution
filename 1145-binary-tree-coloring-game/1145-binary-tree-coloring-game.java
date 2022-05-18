45270a3eefa7dd74c7e409568c2d0aeb3a9e3504
Abhishek Chaubey <90348147+abhi98chaubey@users.noreply.github.com>
2022-05-18 23:52:37 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (61.93%) - LeetHub

---
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public int xleft;
+    public int xright;
+    
+    public int size(TreeNode node, int x) {
+        if(node == null) {
+            return 0;
+        }
+        
+        int ls = size(node.left, x);
+        int rs = size(node.right, x);
+        
+        if(node.val == x){
+            xleft = ls;
+            xright = rs;
+        }
+        
+        return ls + rs + 1;
+    }
+    
+    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+        size(root, x);
+        int otherSize = n - xleft - xright - 1;
+        int max = Math.max(otherSize, Math.max(xleft, xright));
+        
+        int rest = n - max;
+        if(max > rest) {
+            return true;
+        }
+        
+        return false;
+    }
+}
