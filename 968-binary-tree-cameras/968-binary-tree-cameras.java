56df9c0dc9091b8df1111ec3cd1294965859616e
Abhishek Chaubey <90348147+abhi98chaubey@users.noreply.github.com>
2022-06-04 16:20:30 +0530

    Time: 1 ms (73.98%), Space: 43.2 MB (59.33%) - LeetHub

---
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    final int CAMERA = 1;
+    final int MONITORED = 2;
+    final int NOT_MONITORED = 3;
+
+    public int minCameraCover(TreeNode root) {
+     int val = helper(root);
+
+     if(val == NOT_MONITORED){
+         count++;
+     }
+     
+     return count;   
+    }
+
+    public int helper(TreeNode node){
+        if(node == null){
+            return MONITORED;
+        }
+
+        int left = helper(node.left);
+        int right = helper(node.right);
+
+        if(left == NOT_MONITORED || right == NOT_MONITORED){
+            count++;
+            return CAMERA;
+        } else if(left == CAMERA || right == CAMERA){
+            return MONITORED;
+        } else {
+            return NOT_MONITORED;
+        }
+    }
+
+    
+}
